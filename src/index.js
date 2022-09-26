47dafb86178f7ec404cc0eeeec13a7b87422539a
suraj chapagain <surajchapagain7743@gmail.com>
2022-09-26 22:30:28 +0545

    ...

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const server = http.createServer(app);
 app.use(express.static("./public"));
 
 const io = require("socket.io")(server);
-io.on("connection", (socket) => {});
+io.on("connection", (socket) => {
+  socket.on("username", (name) => {
+    socket.emit("takeUsername", () => {
+      return name;
+    });
+  });
+});
 
 server.listen(8000, () => {
   console.log("listening to port 8000");
